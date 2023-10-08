601aff4df062271bc29a25185570702b28ef1d87
Riazul Islam <riazul5207@gmail.com>
2023-10-08 14:07:40 +0600

    navbar ui completed💥

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Topbar from '../sections/Topbar';
 import Header from '../sections/Header';
+import Navbar from '../sections/Navbar';
 
 const Home = () => {
     return (
         <div>
             <Topbar/>
             <Header/>
+            <Navbar/>
         </div>
     );
 }
