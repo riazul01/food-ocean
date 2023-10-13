ddf3b6c29d8c5585886607c03730b4d8fa48dbe1
Riazul Islam <riazul5207@gmail.com>
2023-10-13 12:14:51 +0600

    category section updated🚀

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -17,9 +17,9 @@ const ProductCard = () => {
                 <p className="pt-[0.5rem] text-[1.1rem] text-[#2e7016] font-bold">160 Tk</p>
                 <button className="absolute left-0 bottom-0 py-[0.4rem] w-[45%] bg-[#327e16] text-[1.1rem] text-white font-[500] outline-none rounded-sm">Added</button>
                 <div className={`absolute right-0 bottom-0 flex items-center`}>
-                    <button className="px-[0.7rem] py-[0.62rem] bg-[#222] rounded-sm"><HiMinus className="text-white text-[1.2rem]"/></button>
+                    <button className="px-[0.7rem] py-[0.62rem] border-none outline-none bg-[#222] rounded-sm"><HiMinus className="text-white text-[1.2rem]"/></button>
                     <span className="text-[1.2rem] text-center font-bold w-[2.5rem]">12</span>
-                    <button className="px-[0.7rem] py-[0.62rem] bg-[#222] rounded-sm"><HiPlus className="text-white text-[1.2rem]"/></button>
+                    <button className="px-[0.7rem] py-[0.62rem] border-none outline-none bg-[#222] rounded-sm"><HiPlus className="text-white text-[1.2rem]"/></button>
                 </div>
             </div>
             <p className="absolute top-[1.4rem] left-0 py-[0.2rem] px-[0.8rem] text-[1.1rem] font-bold bg-[orange] rounded-r-[0.1rem]" style={{boxShadow: '0.015rem 0.015rem 0.4rem 0 #444'}}>20% off</p>
--- src/sections/Categories.jsx
+++ src/sections/Categories.jsx
@@ -1,40 +1,41 @@
 import React from 'react';
 import Category from '../components/Category';
 
-// images
-import FastFoodImg from '../assets/images/fast-food.png';
-import MealImg from '../assets/images/meal.png';
-import GrillImg from '../assets/images/grill.png';
-import NaanImg from '../assets/images/naan.png';
-import RiceImg from '../assets/images/rice.png';
-import CurryImg from '../assets/images/curry.png';
-import SaladImg from '../assets/images/salad.png';
-import BiryaniImg from '../assets/images/biryani.png';
-import JuicesImg from '../assets/images/juices.png';
-import ChowmeinImg from '../assets/images/chowmein.png';
-import SoupImg from '../assets/images/soup.png';
-import SweetsImg from '../assets/images/sweets.png';
-import FishImg from '../assets/images/fish.png';
-import MeatImg from '../assets/images/meat.png';
+import VegetableImg from '../assets/images/categories/vegetable.png';
+import FruitImg from '../assets/images/categories/food.png';
+import MeatImg from '../assets/images/categories/meat.png';
+import FishImg from '../assets/images/categories/fish.png';
+import EggImg from '../assets/images/categories/eggs.png';
+import TeaImg from '../assets/images/categories/tea.png';
+import SpicyImg from '../assets/images/categories/spicy.png';
+import DryFruitImg from '../assets/images/categories/dried-fruits.png';
+import JamImg from '../assets/images/categories/jam.png';
+import BiscuitsImg from '../assets/images/categories/cookies.png';
+import CakeImg from '../assets/images/categories/cake.png';
+import BreadImg from '../assets/images/categories/breads.png';
+import FlourImg from '../assets/images/categories/flour.png';
+import RiceImg from '../assets/images/categories/rice.png';
+import OilImg from '../assets/images/categories/oil.png';
 
 const Categories = () => {
     return (
         <div className="mx-auto px-[0.4rem] max-w-[1420px]">
             <div className="w-full grid grid-cols-8 border-[1px] border-e-0 border-b-0 border-[silver]">
-                <Category Category="Fast Foods" CategoryImg={FastFoodImg}/>
-                <Category Category="Meals" CategoryImg={MealImg}/>
-                <Category Category="Kabab & Grills" CategoryImg={GrillImg}/>
-                <Category Category="Parata & Naan" CategoryImg={NaanImg}/>
-                <Category Category="Rice" CategoryImg={RiceImg}/>
-                <Category Category="Curry" CategoryImg={CurryImg}/>
-                <Category Category="Salad" CategoryImg={SaladImg}/>
-                <Category Category="Biryani" CategoryImg={BiryaniImg}/>
-                <Category Category="Juices" CategoryImg={JuicesImg}/>
-                <Category Category="Chowmein" CategoryImg={ChowmeinImg}/>
-                <Category Category="Soup" CategoryImg={SoupImg}/>
-                <Category Category="Dairy & Sweets" CategoryImg={SweetsImg}/>
-                <Category Category="Fish" CategoryImg={FishImg}/>
+                <Category Category="Vegetables" CategoryImg={VegetableImg}/>
+                <Category Category="Fruits" CategoryImg={FruitImg}/>
                 <Category Category="Meat" CategoryImg={MeatImg}/>
+                <Category Category="Fish" CategoryImg={FishImg}/>
+                <Category Category="Eggs" CategoryImg={EggImg}/>
+                <Category Category="Tea & Coffe" CategoryImg={TeaImg}/>
+                <Category Category="Spicies" CategoryImg={SpicyImg}/>
+                <Category Category="Dried Fruits" CategoryImg={DryFruitImg}/>
+                <Category Category="Jams & Jellies" CategoryImg={JamImg}/>
+                <Category Category="Biscuits" CategoryImg={BiscuitsImg}/>
+                <Category Category="Cakes" CategoryImg={CakeImg}/>
+                <Category Category="Breads" CategoryImg={BreadImg}/>
+                <Category Category="Rice" CategoryImg={RiceImg}/>
+                <Category Category="Flour" CategoryImg={FlourImg}/>
+                <Category Category="Oil" CategoryImg={OilImg}/>
             </div>
         </div>
     );
