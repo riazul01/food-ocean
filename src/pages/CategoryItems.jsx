aa98d67926edbaaf5a15f426c20652c74b8beed5
Riazul Islam <riazul5207@gmail.com>
2023-10-15 20:59:46 +0600

    category page completed💥

--- src/pages/CategoryItems.jsx
+++ src/pages/CategoryItems.jsx
@@ -1,4 +1,5 @@
 import React, { useContext } from 'react';
+import ProductCard from '../components/ProductCard';
 import AppLayout from '../layouts/AppLayout';
 import { useLocation } from 'react-router-dom';
 import { ProductsContext } from '../context/ProductContextProvider';
@@ -9,7 +10,17 @@ const CategoryItems = () => {
 
     return (
         <AppLayout>
-            <h1>Hello</h1>
+            <div className="mx-auto px-[0.4rem] pb-[3rem] max-w-[1420px]">
+                <div className="py-[1.2rem] flex items-center justify-between">
+                    <h1 className="text-[1.4rem] font-bold capitalize">{path}</h1>
+                    <p className="text-[1rem] font-bold text-[green]">120 Items</p>
+                </div>
+                <div className="grid grid-cols-5 gap-[1.2rem]">
+                    {fruits.map((item) => {
+                        return <ProductCard key={item.id} data={item}/>
+                    })}
+                </div>
+            </div>
         </AppLayout>
     );
 }
--- src/sections/Navbar.jsx
+++ src/sections/Navbar.jsx
@@ -10,15 +10,15 @@ const Navbar = () => {
                     <NavLink to="/" className="navLink mr-[0.4rem] px-[0.6rem] py-[0.2rem] text-[#222] text-[1rem] border-[1px] border-gray-600 rounded-md">Home</NavLink>
                     <NavLink to="/groceries" className="navLink mr-[0.4rem] px-[0.6rem] py-[0.2rem] text-[#222] text-[1rem] border-[1px] border-gray-600 rounded-md">Groceries</NavLink>
                     <NavLink to="/foods" className="navLink mr-[0.4rem] px-[0.6rem] py-[0.2rem] text-[#222] text-[1rem] border-[1px] border-gray-600 rounded-md">Foods</NavLink>
-                    <NavLink to="/vegetables" className="navLink mr-[0.4rem] px-[0.6rem] py-[0.2rem] text-[#222] text-[1rem] border-[1px] border-gray-600 rounded-md">Vegetables</NavLink>
-                    <NavLink to="/fruits" className="navLink mr-[0.4rem] px-[0.6rem] py-[0.2rem] text-[#222] text-[1rem] border-[1px] border-gray-600 rounded-md">Fruits</NavLink>
-                    <NavLink to="/meat" className="navLink mr-[0.4rem] px-[0.6rem] py-[0.2rem] text-[#222] text-[1rem] border-[1px] border-gray-600 rounded-md">Meat</NavLink>
-                    <NavLink to="/fish" className="navLink mr-[0.4rem] px-[0.6rem] py-[0.2rem] text-[#222] text-[1rem] border-[1px] border-gray-600 rounded-md">Fish</NavLink>
-                    <NavLink to="/eggs" className="navLink mr-[0.4rem] px-[0.6rem] py-[0.2rem] text-[#222] text-[1rem] border-[1px] border-gray-600 rounded-md">Eggs</NavLink>
-                    <NavLink to="/tea-coffe" className="navLink mr-[0.4rem] px-[0.6rem] py-[0.2rem] text-[#222] text-[1rem] border-[1px] border-gray-600 rounded-md">Tea & Coffe</NavLink>
-                    <NavLink to="/spices" className="navLink mr-[0.4rem] px-[0.6rem] py-[0.2rem] text-[#222] text-[1rem] border-[1px] border-gray-600 rounded-md">Spices</NavLink>
-                    <NavLink to="/dry-fruits" className="navLink mr-[0.4rem] px-[0.6rem] py-[0.2rem] text-[#222] text-[1rem] border-[1px] border-gray-600 rounded-md">Dry Fruits</NavLink>
-                </div>           
+                    <NavLink to="/groceries/vegetables" className="navLink mr-[0.4rem] px-[0.6rem] py-[0.2rem] text-[#222] text-[1rem] border-[1px] border-gray-600 rounded-md">Vegetables</NavLink>
+                    <NavLink to="/groceries/fruits" className="navLink mr-[0.4rem] px-[0.6rem] py-[0.2rem] text-[#222] text-[1rem] border-[1px] border-gray-600 rounded-md">Fruits</NavLink>
+                    <NavLink to="/groceries/meat" className="navLink mr-[0.4rem] px-[0.6rem] py-[0.2rem] text-[#222] text-[1rem] border-[1px] border-gray-600 rounded-md">Meat</NavLink>
+                    <NavLink to="/groceries/fish" className="navLink mr-[0.4rem] px-[0.6rem] py-[0.2rem] text-[#222] text-[1rem] border-[1px] border-gray-600 rounded-md">Fish</NavLink>
+                    <NavLink to="/groceries/eggs" className="navLink mr-[0.4rem] px-[0.6rem] py-[0.2rem] text-[#222] text-[1rem] border-[1px] border-gray-600 rounded-md">Eggs</NavLink>
+                    <NavLink to="/groceries/tea-coffe" className="navLink mr-[0.4rem] px-[0.6rem] py-[0.2rem] text-[#222] text-[1rem] border-[1px] border-gray-600 rounded-md">Tea & Coffe</NavLink>
+                    <NavLink to="/groceries/spices" className="navLink mr-[0.4rem] px-[0.6rem] py-[0.2rem] text-[#222] text-[1rem] border-[1px] border-gray-600 rounded-md">Spices</NavLink>
+                    <NavLink to="/groceries/dry-fruits" className="navLink mr-[0.4rem] px-[0.6rem] py-[0.2rem] text-[#222] text-[1rem] border-[1px] border-gray-600 rounded-md">Dry Fruits</NavLink>
+                </div>
             </div>
         </div>
     );
