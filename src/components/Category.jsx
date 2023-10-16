b991e74c1b79360e7ff9cc01d9dd0eedb3194dbf
Riazul Islam <riazul5207@gmail.com>
2023-10-16 12:14:40 +0600

    categories section updated🚀

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 
-const Category = ({ Category="Meals", CategoryImg }) => {
+const Category = ({ title, quantity, Image }) => {
     return (
         <div className="w-full h-[180px] bg-[#fff] border-[1px] border-s-0 border-t-0 border-[silver] cursor-pointer">
             <div className="pt-[1.2rem] pb-[0.6rem] w-full h-[60%]">
-                <img src={CategoryImg} className="w-full h-full object-contain" alt="" />
+                <img src={Image} className="w-full h-full object-contain" alt="" />
             </div>
             <div className=" mt-[0.4rem] w-full h-[40%]">
-                <h1 className="text-center">{Category}</h1>
-                <p className="text-center">20 Items</p>
+                <h1 className="text-center">{title}</h1>
+                <p className="text-center">{quantity} Items</p>
             </div>
         </div>
     );
--- src/sections/Categories.jsx
+++ src/sections/Categories.jsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
 import Category from '../components/Category';
+import { ProductsContext } from '../context/ProductContextProvider';
 
 import VegetableImg from '../assets/images/categories/vegetable.png';
 import FruitImg from '../assets/images/categories/food.png';
@@ -19,53 +20,55 @@ import RiceImg from '../assets/images/categories/rice.png';
 import OilImg from '../assets/images/categories/oil.png';
 
 const Categories = () => {
+    const { vegetables, fruits, meat, fish, eggs, teaCoffe, spices, dryFruits, biscuits, cake, jams, breads, rice, flour, oil } = useContext(ProductsContext);
+
     return (
         <div className="mx-auto pb-[1.6rem] px-[0.4rem] max-w-[1420px]">
             <div className="w-full grid grid-cols-8 border-[1px] border-e-0 border-b-0 border-[silver]">
                 <Link to="/groceries/vegetables">
-                    <Category Category="Vegetables" CategoryImg={VegetableImg}/>
+                    <Category title="Vegetables" quantity={vegetables ? vegetables.length : '0'} Image={VegetableImg}/>
                 </Link>
                 <Link to="/groceries/fruits">
-                    <Category Category="Fruits" CategoryImg={FruitImg}/>
+                    <Category title="Fruits" quantity={fruits ? fruits.length : '0'} Image={FruitImg}/>
                 </Link>
                 <Link to="/groceries/meat">
-                    <Category Category="Meat" CategoryImg={MeatImg}/>
+                    <Category title="Meat" quantity={meat ? meat.length : '0'} Image={MeatImg}/>
                 </Link>
                 <Link to="/groceries/fish">
-                    <Category Category="Fish" CategoryImg={FishImg}/>
+                    <Category title="Fish" quantity={fish ? fish.length : '0'} Image={FishImg}/>
                 </Link>
                 <Link to="/groceries/eggs">
-                    <Category Category="Eggs" CategoryImg={EggImg}/>
+                    <Category title="Eggs" quantity={eggs ? eggs.length : '0'} Image={EggImg}/>
                 </Link>
                 <Link to="/groceries/tea-coffe">
-                    <Category Category="Tea & Coffe" CategoryImg={TeaImg}/>
+                    <Category title="Tea & Coffe" quantity={teaCoffe ? teaCoffe.length : '0'} Image={TeaImg}/>
                 </Link>
                 <Link to="/groceries/spices">
-                    <Category Category="Spicies" CategoryImg={SpicyImg}/>
+                    <Category title="Spicies" quantity={spices ? spices.length : '0'} Image={SpicyImg}/>
                 </Link>
                 <Link to="/groceries/dried-fruits">
-                    <Category Category="Dried Fruits" CategoryImg={DryFruitImg}/>
+                    <Category title="Dried Fruits" quantity={dryFruits ? dryFruits.length : '0'} Image={DryFruitImg}/>
                 </Link>
                 <Link to="/groceries/jams">
-                    <Category Category="Jams & Jellies" CategoryImg={JamImg}/>
+                    <Category title="Jams & Jellies" quantity={jams ? jams.length : '0'} Image={JamImg}/>
                 </Link>
                 <Link to="/groceries/biscuits">
-                    <Category Category="Biscuits" CategoryImg={BiscuitsImg}/>
+                    <Category title="Biscuits" quantity={biscuits ? biscuits.length : '0'} Image={BiscuitsImg}/>
                 </Link>
                 <Link to="/groceries/cakes">
-                    <Category Category="Cakes" CategoryImg={CakeImg}/>
+                    <Category title="Cakes" quantity={cake ? cake.length : '0'} Image={CakeImg}/>
                 </Link>
                 <Link to="/groceries/breads">
-                    <Category Category="Breads" CategoryImg={BreadImg}/>
+                    <Category title="Breads" quantity={breads ? breads.length : '0'} Image={BreadImg}/>
                 </Link>
                 <Link to="/groceries/reice">
-                    <Category Category="Rice" CategoryImg={RiceImg}/>
+                    <Category title="Rice" quantity={rice ? rice.length : '0'} Image={RiceImg}/>
                 </Link>
                 <Link to="/groceries/flour">
-                    <Category Category="Flour" CategoryImg={FlourImg}/>
+                    <Category title="Flour" quantity={flour ? flour.length : '0'} Image={FlourImg}/>
                 </Link>
                 <Link to="/groceries/oil">
-                    <Category Category="Oil" CategoryImg={OilImg}/>
+                    <Category title="Oil" quantity={oil ? oil.length : '0'} Image={OilImg}/>
                 </Link>
             </div>
         </div>
