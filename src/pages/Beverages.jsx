ed00ad6affccaa301eba804a1946e0d65b83b401
Riazul Islam <riazul5207@gmail.com>
2023-10-21 00:05:30 +0600

    beverages page completed💥

--- src/pages/Beverages.jsx
+++ src/pages/Beverages.jsx
@@ -1,10 +1,24 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import AppLayout from '../layouts/AppLayout';
+import Products from '../sections/Products';
+
+import { ProductsContext } from '../context/ProductContextProvider';
+import Categories from '../sections/Categories';
 
 const Beverages = () => {
+    const { teaCoffe, dryFruits, biscuits, cake, jams, breads } = useContext(ProductsContext);
+
     return (
         <AppLayout>
-            <h1>Hello</h1>
+            <div className="mx-auto max-w-[1420px]">
+                <Categories/>
+                <Products title="tea & coffe" path="/groceries/beverages/tea-coffe" products={teaCoffe}/>
+                <Products title="dried fruits" path="/groceries/beverages/dried-fruits" products={dryFruits}/>
+                <Products title="jams & jellies" path="/groceries/beverages/jams" products={jams}/>
+                <Products title="biscuits" path="/groceries/beverages/biscuits" products={biscuits}/>
+                <Products title="cakes" path="/groceries/beverages/cakes" products={cake}/>
+                <Products title="breads" path="/groceries/raw-products/breads" products={breads}/>           
+            </div>
         </AppLayout>
     );
 }
