a6ba6c39e76baab5ebef5e65c40ef00db280a1b1
Riazul Islam <riazul5207@gmail.com>
2023-10-20 23:41:38 +0600

    raw products page completed💥

--- src/pages/RawProducts.jsx
+++ src/pages/RawProducts.jsx
@@ -1,10 +1,33 @@
-import React from 'react';
+import React, { useContext }  from 'react';
 import AppLayout from '../layouts/AppLayout';
+import Products from '../sections/Products';
+
+import { ProductsContext } from '../context/ProductContextProvider';
+import Categories from '../sections/Categories';
 
 const RawProducts = () => {
+    const { vegetables, fruits, meat, fish, eggs, teaCoffe, spices, dryFruits, biscuits, cake, jams, breads, rice, flour, oil } = useContext(ProductsContext);
+
     return (
         <AppLayout>
-            <h1>Hello!</h1>
+            <div className="mx-auto max-w-[1420px]">
+                <Categories/>
+                <Products title="vegetables" path="/groceries/raw-products/vegetables" products={vegetables}/>
+                <Products title="fruits" path="/groceries/raw-products/fruits" products={fruits}/>
+                <Products title="meat" path="/groceries/raw-products/meat" products={meat}/>
+                <Products title="fish" path="/groceries/raw-products/fish" products={fish}/>
+                <Products title="eggs" path="/groceries/raw-products/eggs" products={eggs}/>
+                <Products title="tea & coffe" path="/groceries/beverages/tea-coffe" products={teaCoffe}/>
+                <Products title="spices" path="/groceries/raw-products/spices" products={spices}/>
+                <Products title="dried fruits" path="/groceries/beverages/dried-fruits" products={dryFruits}/>
+                <Products title="jams & jellies" path="/groceries/beverages/jams" products={jams}/>
+                <Products title="biscuits" path="/groceries/beverages/biscuits" products={biscuits}/>
+                <Products title="cakes" path="/groceries/beverages/cakes" products={cake}/>
+                <Products title="breads" path="/groceries/raw-products/breads" products={breads}/>
+                <Products title="rice" path="/groceries/raw-products/rice" products={rice}/>
+                <Products title="flour" path="/groceries/raw-products/flour" products={flour}/>
+                <Products title="oil" path="/groceries/raw-products/oil" products={oil}/>            
+            </div>
         </AppLayout>
     );
 }
