b89b79735beb4532d56b9800339e0b6e0c9f661e
Riazul Islam <riazul5207@gmail.com>
2023-10-22 09:30:10 +0600

    footer links updated✈️

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -28,36 +28,36 @@ const Footer = () => {
 
                 <div className="flex flex-col w-[18%]">
                     <h1 className="text-[orange] text-[1.2rem] font-bold">Navigation</h1>
-                    <Link to="" className="mt-[0.6rem] text-[#fff] text-[1.1rem]">Home</Link>
-                    <Link to="" className="mt-[0.6rem] text-[#fff] text-[1.1rem]">About Us</Link>
-                    <Link to="" className="mt-[0.6rem] text-[#fff] text-[1.1rem]">Groceries</Link>
-                    <Link to="" className="mt-[0.6rem] text-[#fff] text-[1.1rem]">Raw Products</Link>
-                    <Link to="" className="mt-[0.6rem] text-[#fff] text-[1.1rem]">Beverages</Link>
+                    <Link to="/" className="mt-[0.6rem] text-[#fff] text-[1.1rem]">Home</Link>
+                    <Link to="/about" className="mt-[0.6rem] text-[#fff] text-[1.1rem]">About Us</Link>
+                    <Link to="/groceries" className="mt-[0.6rem] text-[#fff] text-[1.1rem]">Groceries</Link>
+                    <Link to="/groceries/raw-products" className="mt-[0.6rem] text-[#fff] text-[1.1rem]">Raw Products</Link>
+                    <Link to="/groceries/beverages" className="mt-[0.6rem] text-[#fff] text-[1.1rem]">Beverages</Link>
                 </div>
 
                 <div className="flex flex-col w-[18%]">
                     <h1 className="text-[orange] text-[1.2rem] font-bold">Trending</h1>
-                    <Link to="" className="mt-[0.6rem] text-[#fff] text-[1.1rem]">Fruits</Link>
-                    <Link to="" className="mt-[0.6rem] text-[#fff] text-[1.1rem]">Vegetables</Link>
-                    <Link to="" className="mt-[0.6rem] text-[#fff] text-[1.1rem]">Top Products</Link>
-                    <Link to="" className="mt-[0.6rem] text-[#fff] text-[1.1rem]">Recent Products</Link>
-                    <Link to="" className="mt-[0.6rem] text-[#fff] text-[1.1rem]">Popular Now</Link>
+                    <Link to="/groceries/raw-products/fruits" className="mt-[0.6rem] text-[#fff] text-[1.1rem]">Fruits</Link>
+                    <Link to="/groceries/raw-products/vegetables" className="mt-[0.6rem] text-[#fff] text-[1.1rem]">Vegetables</Link>
+                    <Link to="/groceries/top-products" className="mt-[0.6rem] text-[#fff] text-[1.1rem]">Top Products</Link>
+                    <Link to="/groceries/recent-products" className="mt-[0.6rem] text-[#fff] text-[1.1rem]">Recent Products</Link>
+                    <Link to="/groceries/popular-now" className="mt-[0.6rem] text-[#fff] text-[1.1rem]">Popular Now</Link>
                 </div>
                 
                 <div className="flex flex-col w-[18%]">
                     <h1 className="text-[orange] text-[1.2rem] font-bold">Quick Links</h1>
-                    <Link to="" className="mt-[0.6rem] text-[#fff] text-[1.1rem]">User Account</Link>
-                    <Link to="" className="mt-[0.6rem] text-[#fff] text-[1.1rem]">Become an Affilate</Link>
-                    <Link to="" className="mt-[0.6rem] text-[#fff] text-[1.1rem]">Track Orders</Link>
-                    <Link to="" className="mt-[0.6rem] text-[#fff] text-[1.1rem]">New offers</Link>
-                    <Link to="" className="mt-[0.6rem] text-[#fff] text-[1.1rem]">Latest Blogs</Link>
+                    <Link to="/profile" className="mt-[0.6rem] text-[#fff] text-[1.1rem]">User Account</Link>
+                    <Link to="/" className="mt-[0.6rem] text-[#fff] text-[1.1rem]">Become an Affilate</Link>
+                    <Link to="/" className="mt-[0.6rem] text-[#fff] text-[1.1rem]">Track Orders</Link>
+                    <Link to="/" className="mt-[0.6rem] text-[#fff] text-[1.1rem]">New offers</Link>
+                    <Link to="/" className="mt-[0.6rem] text-[#fff] text-[1.1rem]">Latest Blogs</Link>
                 </div>
 
                 <div className="flex flex-col w-[18%]">
                     <h1 className="text-[orange] text-[1.2rem] font-bold">Services</h1>
-                    <Link to="" className="mt-[0.6rem] text-[#fff] text-[1.1rem]">Help Center</Link>
-                    <Link to="" className="mt-[0.6rem] text-[#fff] text-[1.1rem]">Terms of use</Link>
-                    <Link to="" className="mt-[0.6rem] text-[#fff] text-[1.1rem]">Privacy policy</Link>
+                    <Link to="/" className="mt-[0.6rem] text-[#fff] text-[1.1rem]">Help Center</Link>
+                    <Link to="/" className="mt-[0.6rem] text-[#fff] text-[1.1rem]">Terms of use</Link>
+                    <Link to="/" className="mt-[0.6rem] text-[#fff] text-[1.1rem]">Privacy policy</Link>
                 </div>
             </div>
         </div>
