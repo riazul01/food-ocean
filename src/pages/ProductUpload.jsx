0cd3b9bcdaf8dbb539dea6fd9d0977cba7652fdc
Riazul Islam <riazul5207@gmail.com>
2023-10-23 10:49:25 +0600

    product updload page updated🚀

--- src/pages/ProductUpload.jsx
+++ src/pages/ProductUpload.jsx
@@ -196,7 +196,7 @@ const AddProductForm = () => {
                         <option value="spices">Spices</option>
                         <option value="dry-fruits">Dry Fruits</option>
                         <option value="biscuits">Biscuits</option>
-                        <option value="cake">Cakes</option>
+                        <option value="cakes">Cakes</option>
                         <option value="jams">Jams & Jellies</option>
                         <option value="breads">Breads</option>
                         <option value="rice">Rice</option>
