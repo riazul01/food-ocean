8b0b29630feb096981f408abfb452c682c3f3d46
Riazul Islam <riazul5207@gmail.com>
2023-10-26 15:24:08 +0600

    login context created🔨

---
+++ src/context/LoginContextProvider.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const LoginContextProvider = () => {
+    return (
+        <div>
+            <h1>Hello</h1>
+        </div>
+    );
+}
+
+export default LoginContextProvider;
