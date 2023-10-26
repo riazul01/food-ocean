f05f34951557c0c7df7bd0274598613920e9643f
Riazul Islam <riazul5207@gmail.com>
2023-10-26 15:28:57 +0600

    login context completed💥

--- src/context/LoginContextProvider.jsx
+++ src/context/LoginContextProvider.jsx
@@ -1,10 +1,40 @@
-import React from 'react';
+import React, { createContext, useEffect, useReducer } from 'react';
 
-const LoginContextProvider = () => {
+export const LoginContext = createContext();
+
+const reducer = (state, action) => {
+    switch(action.type) {
+        case 'LOGIN':
+            return {currentUser: action.payload}
+        case 'LOGOUT':
+            return {currentUser: null}
+        default:
+            return state;
+    }
+}
+
+const INITIAL_STATE = {
+    currentUser: JSON.parse(localStorage.getItem("user")) || null
+}
+
+const LoginContextProvider = ({ children }) => {
+    const [state, dispatch] = useReducer(reducer, INITIAL_STATE);
+
+    useEffect(() => {
+        if (state.currentUser) {
+            localStorage.setItem("user", JSON.stringify(state.currentUser));
+        }
+
+        if (state.currentUser === null) {
+            localStorage.removeItem("user");
+            localStorage.removeItem("userDetails");
+        }
+    }, [state.currentUser]);
+
     return (
-        <div>
-            <h1>Hello</h1>
-        </div>
+        <LoginContext.Provider value={{currentUser: state.currentUser, dispatch}}>
+            {children}
+        </LoginContext.Provider>
     );
 }
 
