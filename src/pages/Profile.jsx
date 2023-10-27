83dc8b18bde7543f50e35a19b5ded7ab931ebe36
Riazul Islam <riazul5207@gmail.com>
2023-10-27 19:52:06 +0600

    profile page completed💥

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -6,34 +6,43 @@ import ProfileLayout from '../layouts/ProfileLayout';
 import { SlLocationPin } from 'react-icons/sl';
 import { FiCalendar } from 'react-icons/fi';
 
-import TestImg from '../assets/images/man.png';
+import TestImg from '../assets/images/user.png';
 
 const Profile = () => {
     return (
         <AppLayout>
             <ProfileLayout>
-                <div className="">
-                    {/* title */}
-                    <h1 className="text-[#173334] text-[1.6rem] font-[500] border-b-[1px] border-dashed border-[#ddd]">My Profile</h1>
-                    
-                    {/* header */}
-                    <div className="mt-[1rem] flex items-end justify-start gap-[1rem]">
-                        <div className="h-[140px] w-140px] border-[5px] border-[#333] rounded-md">
-                            <img src={TestImg} className="h-full w-full object-cover" alt="" />
+                {/* title */}
+                <h1 className="pb-[0.2rem] text-[#173334] text-[1.6rem] font-[500] border-b-[1px] border-dashed border-[#ddd]">My Profile</h1>
+                
+                {/* header */}
+                <div className="mt-[1rem] flex items-end justify-start gap-[1rem]">
+                    <div className="h-[140px] w-140px] rounded-md overflow-hidden">
+                        <img src={TestImg} className="h-full w-full object-cover" alt="" />
+                    </div>
+                    <div className="">
+                        <h1 className="text-[1.6rem] font-bold">Mr. Jhon</h1>
+                        <div className="flex items-center">
+                            <SlLocationPin className="text-[1.1rem]"/>
+                            <p className="mt-[0.2rem] ml-[0.3rem] text-[1.1rem]">Sylhet, Bangladesh</p>
                         </div>
-                        <div className="">
-                            <h1 className="text-[1.6rem] font-bold">Mr. Jhon</h1>
-                            <div className="flex items-center">
-                                <SlLocationPin className="text-[1.1rem]"/>
-                                <p className="mt-[0.2rem] ml-[0.3rem] text-[1.1rem]">Sylhet, Bangladesh</p>
-                            </div>
-                            <div className="flex items-center">
-                                <FiCalendar className="text-[1.1rem]"/>
-                                <p className="mt-[0.2rem] ml-[0.3rem] text-[1.1rem]">Joined - November, 2023</p>
-                            </div>
+                        <div className="flex items-center">
+                            <FiCalendar className="text-[1.1rem]"/>
+                            <p className="mt-[0.2rem] ml-[0.3rem] text-[1.1rem]">Joined - November, 2023</p>
                         </div>
                     </div>
                 </div>
+
+                {/* description */}
+                <div className="mt-[2rem]">
+                    <p className="mt-[0.8rem] flex items-center"><strong className="text-[1.1rem] w-[120px]">Email:</strong><span className="text-[1.1rem]">hello@gmail.com</span></p>
+                    <p className="mt-[0.8rem] flex items-center"><strong className="text-[1.1rem] w-[120px]">Phone:</strong><span className="text-[1.1rem]">+8801234567890</span></p>
+                    <p className="mt-[0.8rem] flex items-center"><strong className="text-[1.1rem] w-[120px]">Gender:</strong><span className="text-[1.1rem]">Male</span></p>
+                    <p className="mt-[0.8rem] flex items-center"><strong className="text-[1.1rem] w-[120px]">Address:</strong><span className="text-[1.1rem]">Alurtol road, Tilagor, Sylhet</span></p>
+                </div>
+
+                {/* delete account button */}
+                <button className="mt-[2rem] px-[0.6rem] py-[0.2rem] text-[#a71818] font-[500] border-[1px] border-[#a71818] rounded-md">Delete account</button>
             </ProfileLayout>
         </AppLayout>
     );
