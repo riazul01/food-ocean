e519c153cb95680c15d94340d4394a5341f41e7a
Riazul Islam <riazul5207@gmail.com>
2023-10-27 09:30:13 +0600

    signup page ui updated🚀

--- src/auth/Signup.jsx
+++ src/auth/Signup.jsx
@@ -8,7 +8,7 @@ import { doc, setDoc } from "firebase/firestore";
 import { createUserWithEmailAndPassword } from "firebase/auth";
 
 // icons
-import { AiFillWarning } from 'react-icons/ai';
+import { PiWarningCircleDuotone } from 'react-icons/pi';
 import { SiMaildotru } from 'react-icons/si';
 import { BiSolidLock } from 'react-icons/bi';
 import { LuUserCircle2 } from 'react-icons/lu';
@@ -81,15 +81,13 @@ const Signup = () => {
             <div className="mx-auto pb-[1.4rem] max-w-[1420px] flex items-center justify-center">
                 <div className="my-[1rem] w-[380px] h-auto min-h-[50vh]">
                     <h1 className="text-[#173334] text-[1.8rem]">Register</h1>
-                    <p className="text-[#182828] text-[1.1rem]">Please create an account to continue!</p>
-                    {error.flag && <div className="errorBox">
-                        <div className="errorIcon">
-                            <AiFillWarning className="warning"/>
-                        </div>
-                        <p className="errorMsg">{error.message}</p>
+                    <p className="mt-[0.2rem] text-[#182828] text-[1.1rem]">Please create an account to continue!</p>
+                    {error.flag && <div className="mt-[0.3rem] flex items-center">
+                        <PiWarningCircleDuotone className="text-[#b12525] text-[1.3rem]"/>
+                        <p className="ml-[0.3rem] text-[#b12525] text-[1.1rem]">{error.message}</p>
                     </div>}
                     <form onSubmit={handleSubmit}>
-                        <div className="mt-[1rem] w-full h-[45px] flex items-center border-[1px] border-[#ddd] rounded-md overflow-hidden">
+                        <div className="mt-[0.8rem] w-full h-[45px] flex items-center border-[1px] border-[#ddd] rounded-md overflow-hidden">
                             <SiMaildotru className="ms-[0.8rem] text-gray-400 text-[1rem]"/>
                             <input type="email" name="email" value={user.email} onChange={handleChange} className="px-[0.65rem] h-full w-full border-none outline-none box-border" placeholder="Enter your email" required/>
                         </div>
@@ -104,8 +102,8 @@ const Signup = () => {
                         <button type="submit" className="mt-[1.2rem] px-[1rem] py-[0.4rem] text-[#fff] font-[500] bg-[#173334] rounded-lg">Submit</button>
                     </form>
                     <div className="mt-[1rem] flex items-center">
-                        <p className="me-[0.6rem] text-[1.1rem]">Already have an account?</p>
-                        <Link to="/user/login" className="text-[1.1rem] underline">Login now</Link>
+                        <p className="me-[0.6rem] text-[#182828] text-[1.1rem]">Already have an account?</p>
+                        <Link to="/user/login" className="text-[#182828] text-[1.1rem] underline">Login now</Link>
                     </div>
                 </div>
             </div>
