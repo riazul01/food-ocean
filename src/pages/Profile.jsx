6f62376f843ae413ba328e63df9c40fe0796ac83
Riazul Islam <riazul5207@gmail.com>
2023-10-27 18:34:26 +0600

    profile header completed💥

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -2,11 +2,38 @@ import React from 'react';
 import AppLayout from '../layouts/AppLayout';
 import ProfileLayout from '../layouts/ProfileLayout';
 
+// icons
+import { SlLocationPin } from 'react-icons/sl';
+import { FiCalendar } from 'react-icons/fi';
+
+import TestImg from '../assets/images/man.png';
+
 const Profile = () => {
     return (
         <AppLayout>
             <ProfileLayout>
-                <h1>Hello!</h1>
+                <div className="">
+                    {/* title */}
+                    <h1 className="text-[#173334] text-[1.6rem] font-[500] border-b-[1px] border-dashed border-[#ddd]">My Profile</h1>
+                    
+                    {/* header */}
+                    <div className="mt-[1rem] flex items-end justify-start gap-[1rem]">
+                        <div className="h-[140px] w-140px] border-[5px] border-[#333] rounded-md">
+                            <img src={TestImg} className="h-full w-full object-cover" alt="" />
+                        </div>
+                        <div className="">
+                            <h1 className="text-[1.6rem] font-bold">Mr. Jhon</h1>
+                            <div className="flex items-center">
+                                <SlLocationPin className="text-[1.1rem]"/>
+                                <p className="mt-[0.2rem] ml-[0.3rem] text-[1.1rem]">Sylhet, Bangladesh</p>
+                            </div>
+                            <div className="flex items-center">
+                                <FiCalendar className="text-[1.1rem]"/>
+                                <p className="mt-[0.2rem] ml-[0.3rem] text-[1.1rem]">Joined - November, 2023</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </ProfileLayout>
         </AppLayout>
     );
