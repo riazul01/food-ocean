6d4b8ed0b7b3dbeeffa6de91276bf6185c917817
Riazul Islam <riazul5207@gmail.com>
2023-10-28 12:37:18 +0600

    profile page updated🚀

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,14 +1,27 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import AppLayout from '../layouts/AppLayout';
 import ProfileLayout from '../layouts/ProfileLayout';
+
+import { UserDetailsContext } from '../context/UserDetailsContextProvider';
 
 // icons
 import { SlLocationPin } from 'react-icons/sl';
 import { FiCalendar } from 'react-icons/fi';
 
-import TestImg from '../assets/images/shopping.png';
+import DefaultImg from '../assets/images/shopping.png';
 
 const Profile = () => {
+    const userDetails = useContext(UserDetailsContext);
+
+    console.log(userDetails);
+
+    let joinedDate = 'Month, YYYY';
+
+    if (userDetails.joinedDate) {
+        let dateArr = userDetails.joinedDate.split(' ');
+        joinedDate = `${dateArr[0]}, ${dateArr[2]}`;
+    }
+
     return (
         <AppLayout>
             <ProfileLayout>
@@ -18,27 +31,27 @@ const Profile = () => {
                 {/* header */}
                 <div className="mt-[1rem] flex items-end justify-start gap-[1rem]">
                     <div className="h-[130px] w-[130px] rounded-md overflow-hidden">
-                        <img src={TestImg} className="h-full w-full object-cover" alt="" />
+                        <img src={userDetails.imgUrl || DefaultImg} className="h-full w-full object-cover" alt="profile" />
                     </div>
                     <div className="">
-                        <h1 className="text-[1.6rem] font-bold">Mr. Jhon</h1>
+                        <h1 className="text-[1.6rem] font-bold">{userDetails ? userDetails.name : 'Mr. X'}</h1>
                         <div className="flex items-center">
                             <SlLocationPin className="text-[1.1rem]"/>
                             <p className="mt-[0.2rem] ml-[0.3rem] text-[1.1rem]">Sylhet, Bangladesh</p>
                         </div>
                         <div className="flex items-center">
                             <FiCalendar className="text-[1.1rem]"/>
-                            <p className="mt-[0.2rem] ml-[0.3rem] text-[1.1rem]">Joined - November, 2023</p>
+                            <p className="mt-[0.2rem] ml-[0.3rem] text-[1.1rem]">Joined - {joinedDate}</p>
                         </div>
                     </div>
                 </div>
 
                 {/* description */}
                 <div className="mt-[2rem]">
-                    <p className="mt-[0.8rem] flex items-center"><strong className="text-[1.1rem] w-[120px]">Email:</strong><span className="text-[1.1rem]">hello@gmail.com</span></p>
-                    <p className="mt-[0.8rem] flex items-center"><strong className="text-[1.1rem] w-[120px]">Phone:</strong><span className="text-[1.1rem]">+8801234567890</span></p>
-                    <p className="mt-[0.8rem] flex items-center"><strong className="text-[1.1rem] w-[120px]">Gender:</strong><span className="text-[1.1rem]">Male</span></p>
-                    <p className="mt-[0.8rem] flex items-center"><strong className="text-[1.1rem] w-[120px]">Address:</strong><span className="text-[1.1rem]">Alurtol road, Tilagor, Sylhet</span></p>
+                    <p className="mt-[0.8rem] flex items-center"><strong className="text-[1.1rem] w-[120px]">Email:</strong><span className="text-[1.1rem]">{userDetails.email ? userDetails.email : ''}</span></p>
+                    <p className="mt-[0.8rem] flex items-center"><strong className="text-[1.1rem] w-[120px]">Phone:</strong><span className="text-[1.1rem]">{userDetails.phone ? userDetails.phone : '-:-'}</span></p>
+                    <p className="mt-[0.8rem] flex items-center"><strong className="text-[1.1rem] w-[120px]">Gender:</strong><span className="text-[1.1rem]">{userDetails.gender ? userDetails.gender : '-:-'}</span></p>
+                    <p className="mt-[0.8rem] flex items-center"><strong className="text-[1.1rem] w-[120px]">Address:</strong><span className="text-[1.1rem]">{`${userDetails.address.division ? userDetails.address.division : 'Dhaka'}, Bangladesh`}</span></p>
                 </div>
 
                 {/* delete account button */}
