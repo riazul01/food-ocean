46bbc5c1f2f5e650125eb64446f0a7d6d844d863
Riazul Islam <riazul5207@gmail.com>
2023-10-28 11:56:28 +0600

    user details context completed💥

--- src/context/UserDetailsContextProvider.jsx
+++ src/context/UserDetailsContextProvider.jsx
@@ -1,10 +1,45 @@
-import React, { createContext } from 'react';
+import React, { useState, useEffect, useContext, createContext } from 'react';
+import { LoginContext } from './LoginContextProvider';
+
+// firebase
+import { doc, onSnapshot } from "firebase/firestore";
+import { fs } from "../firebase";
 
 export const UserDetailsContext = createContext();
 
 const UserDetailsContextProvider = ({ children }) => {
+    const { currentUser } = useContext(LoginContext);
+    const [userDetails, setUserDetails] = useState(JSON.parse(localStorage.getItem('userDetails')) || currentUser);
+
+    useEffect(() => {
+        // get user data from firestore
+        const getUserData = async () => {
+            if (currentUser) {
+                onSnapshot(doc(fs, "users", currentUser.uid), (doc) => {
+                    let userObj = {
+                        id: currentUser.uid,
+                        phone: '',
+                        gender: '',
+                        address: {
+                            street: '',
+                            division: '',
+                            city: '',
+                            postcode: '',
+                            country: 'Bangladesh'
+                        },
+                        ...doc.data(),
+                    }
+                    setUserDetails(userObj);
+                    localStorage.setItem('userDetails', JSON.stringify(userObj));
+                });
+            }    
+        }
+        
+        getUserData();
+    }, [currentUser]);
+
     return (
-        <UserDetailsContext.Provider value={null}>
+        <UserDetailsContext.Provider value={userDetails}>
             {children}
         </UserDetailsContext.Provider>
     );
