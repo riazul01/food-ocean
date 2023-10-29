aea4cc423bb300658baeb6968a7419c48c1a7d2c
Riazul Islam <riazul5207@gmail.com>
2023-10-29 15:13:16 +0600

    update profile loader updated🚀

--- src/pages/UpdateProfile.jsx
+++ src/pages/UpdateProfile.jsx
@@ -180,25 +180,25 @@ const UpdateProfile = () => {
                     {/* profile image */}
                     {userDetails ? <div className="h-[130px] w-[130px] rounded-md overflow-hidden">
                         <img src={previewImage || user.imgUrl || user.defaultImg} className="h-full w-full object-cover" alt="profile"/>
-                    </div> : <Skeleton width={100} height={100}/>}
+                    </div> : <Skeleton className="w-[130px] h-[130px]"/>}
 
                     {/* profile info */}
                     <div className="">
 
                         {/* name */}
-                        <h1 className="text-[1.6rem] font-bold capitalize">{userDetails ? (userDetails.name ? userDetails.name : <Skeleton width={100}/>) : <Skeleton width={100}/>}</h1>
+                        <h1 className="text-[1.6rem] font-bold capitalize">{userDetails ? (userDetails.name ? userDetails.name : <Skeleton className="ml-[0.3rem] w-[120px]"/>) : <Skeleton className="ml-[0.3rem] w-[120px]"/>}</h1>
                         
                         {/* location */}
                         {userDetails ? <div className="flex items-center">
                             <SlLocationPin className="text-[1.1rem]"/>
-                            <p className="mt-[0.2rem] ml-[0.3rem] text-[1.1rem] capitalize">{userDetails.address ? `${userDetails.address.division}, ${userDetails.address.country}` : <Skeleton width={100}/>}</p>
-                        </div> : <Skeleton containerClassName="flex-1" width={100}/>}
+                            <p className="mt-[0.2rem] ml-[0.3rem] text-[1.1rem] capitalize">{userDetails.address ? `${userDetails.address.division}, ${userDetails.address.country}` : <Skeleton className="mb-[0.6rem] w-[130px] h-[22px]"/>}</p>
+                        </div> : <Skeleton containerClassName="flex-1" className="mt-[0.2rem] ml-[0.3rem] w-[120px]"/>}
                         
                         {/* joined date */}
                         {userDetails ? <div className="flex items-center">
                             <FiCalendar className="text-[1.1rem]"/>
-                            <p className="mt-[0.2rem] ml-[0.3rem] text-[1.1rem]">{userDetails.joinedDate ? `Joined - ${userDetails.joinedDate.split(' ')[0]}, ${userDetails.joinedDate.split(' ')[2]}` : <Skeleton width={100}/>}</p>
-                        </div> : <Skeleton containerClassName="flex-1" width={100}/>}
+                            <p className="mt-[0.2rem] ml-[0.3rem] text-[1.1rem]">{userDetails.joinedDate ? `Joined - ${userDetails.joinedDate.split(' ')[0]}, ${userDetails.joinedDate.split(' ')[2]}` : <Skeleton className="mb-[0.6rem] w-[130px] h-[22px]"/>}</p>
+                        </div> : <Skeleton containerClassName="flex-1" className="mt-[0.2rem] ml-[0.3rem] w-[120px]"/>}
                     </div>
                 </div>
 
@@ -206,7 +206,7 @@ const UpdateProfile = () => {
                 {userDetails ? <div className="mt-[1rem]">
                     <label htmlFor="profileImg" className="px-[0.8rem] py-[0.2rem]  bg-[#ddd] border-[1px] border-[#999] rounded-sm">Change Image</label>
                     <input type="file" id="profileImg" onChange={handleImageChange} accept="image/*" className="hidden" />
-                </div> : <Skeleton width={100}/>}
+                </div> : <Skeleton className="mt-[0.8rem] w-[130px] h-[25px]"/>}
 
                 {/* description */}
                 <form onSubmit={handleSubmit} className="mt-[2rem]">
@@ -215,19 +215,19 @@ const UpdateProfile = () => {
                     {userDetails ? <div className="mt-[0.8rem] flex items-center">
                         <strong className="text-[1.1rem] w-[120px]">Name:</strong>
                         <input value={user.name} onChange={handleChange} type="text" name="name" className="px-[0.4rem] py-[0.1rem] text-[1.1rem] w-[280px] border-[1px] border-[silver] outline-none rounded-md" placeholder="Your name" required/>
-                    </div> : <Skeleton containerClassName="flex-1" width={218}/>}
+                    </div> : <Skeleton containerClassName="flex-1" className="mb-[0.6rem] w-[270px] h-[22px]"/>}
 
                     {/* email */}
                     {userDetails ? <div className="mt-[0.8rem] flex items-center">
                         <strong className="text-[1.1rem] w-[120px]">Email:</strong>
                         <input value={user.email} onChange={handleChange} type="email" name="email" className="px-[0.4rem] py-[0.1rem] text-[1.1rem] w-[280px] border-[1px] border-[silver] outline-none rounded-md" placeholder="Your email" disabled/>
-                    </div> : <Skeleton containerClassName="flex-1" width={218}/>}
+                    </div> : <Skeleton containerClassName="flex-1" className="mb-[0.6rem] w-[270px] h-[22px]"/>}
 
                     {/* phone */}
                     {userDetails ? <div className="mt-[0.8rem] flex items-center">
                         <strong className="text-[1.1rem] w-[120px]">Phone:</strong>
                         <input value={user.phone} onChange={handleChange} type="phone" name="phone" className="px-[0.4rem] py-[0.1rem] text-[1.1rem] w-[280px] border-[1px] border-[silver] outline-none rounded-md" placeholder="Phone number" required/>
-                    </div> : <Skeleton containerClassName="flex-1" width={218}/>}
+                    </div> : <Skeleton containerClassName="flex-1" className="mb-[0.6rem] w-[270px] h-[22px]"/>}
 
                     {/* gender */}
                     {userDetails ? <div className="mt-[0.8rem] flex items-center">
@@ -236,28 +236,28 @@ const UpdateProfile = () => {
                             <option value="male">Male</option>
                             <option value="female">Female</option>
                         </select>
-                    </div> : <Skeleton width={100}/>}
+                    </div> : <Skeleton className="mb-[0.6rem] w-[130px] h-[22px]"/>}
 
                     {/* address */}
-                    {userDetails ? <h1 className="mt-[1.1rem] text-[#555] text-[1.2rem] font-bold inline-block">Address</h1> : <Skeleton width={100}/>}
+                    {userDetails ? <h1 className="mt-[1.6rem] text-[#555] text-[1.2rem] font-bold inline-block">Address</h1> : <Skeleton className="mt-[1.6rem] mb-[0.6rem] w-[130px] h-[22px]"/>}
                     
                     {/* street */}
                     {userDetails ? <div className="mt-[0.8rem] flex items-center">
                         <strong className="text-[1.1rem] w-[120px]">Street:</strong>
                         <input value={address.street} onChange={handleAddressChange} type="text" name="street" className="px-[0.4rem] py-[0.1rem] text-[1.1rem] w-[280px] border-[1px] border-[silver] outline-none rounded-md" placeholder="Street" required/>
-                    </div> : <Skeleton containerClassName="flex-1" width={218}/>}
+                    </div> : <Skeleton containerClassName="flex-1" className="mb-[0.6rem] w-[270px] h-[22px]"/>}
 
                     {/* city */}
                     {userDetails ? <div className="mt-[0.8rem] flex items-center">
                         <strong className="text-[1.1rem] w-[120px]">City:</strong>
                         <input value={address.city} onChange={handleAddressChange} type="text" name="city" className="px-[0.4rem] py-[0.1rem] text-[1.1rem] w-[280px] border-[1px] border-[silver] outline-none rounded-md" placeholder="City" required/>
-                    </div> : <Skeleton containerClassName="flex-1" width={218}/>}
+                    </div> : <Skeleton containerClassName="flex-1" className="mb-[0.6rem] w-[270px] h-[22px]"/>}
 
                     {/* postcode */}
                     {userDetails ? <div className="mt-[0.8rem] flex items-center">
                         <strong className="text-[1.1rem] w-[120px]">Postcode:</strong>
                         <input value={address.postcode} onChange={handleAddressChange} type="text" name="postcode" className="px-[0.4rem] py-[0.1rem] text-[1.1rem] w-[280px] border-[1px] border-[silver] outline-none rounded-md" placeholder="Postcode" required/>
-                    </div> : <Skeleton containerClassName="flex-1" width={218}/>}
+                    </div> : <Skeleton containerClassName="flex-1" className="mb-[0.6rem] w-[270px] h-[22px]"/>}
 
                     {/* division */}
                     {userDetails ? <div className="mt-[0.8rem] flex items-center">
@@ -272,16 +272,16 @@ const UpdateProfile = () => {
                             <option value="khulna">Khulna</option>
                             <option value="chattogram">Chattogram</option>
                         </select>
-                    </div> : <Skeleton width={100}/>}
+                    </div> : <Skeleton className="mb-[0.6rem] w-[130px] h-[22px]"/>}
 
                     {/* country */}
                     {userDetails ? <div className="mt-[0.8rem] flex items-center">
                         <strong className="text-[1.1rem] w-[120px]">Country:</strong>
                         <input value={address.country} onChange={handleAddressChange} type="text" name="country" className="px-[0.4rem] py-[0.1rem] text-[1.1rem] w-[280px] border-[1px] border-[silver] outline-none rounded-md" placeholder="Country" disabled/>
-                    </div> : <Skeleton containerClassName="flex-1" width={218}/>}
+                    </div> : <Skeleton containerClassName="flex-1" className="mb-[0.6rem] w-[270px] h-[22px]"/>}
 
                     {/* update button */}
-                    {userDetails ? <button type="submit" className="mt-[2rem] px-[0.6rem] py-[0.2rem] text-[#fff] font-[500] bg-green-800 rounded-md" disabled={btnDisabled}>Update account</button> : <Skeleton width={100}/>}
+                    {userDetails ? <button type="submit" className="mt-[2rem] px-[0.6rem] py-[0.2rem] text-[#fff] font-[500] bg-green-800 rounded-md" disabled={btnDisabled}>Update account</button> : <Skeleton className="mb-[0.6rem] w-[130px] h-[22px]"/>}
                 </form>
                 <ToastContainer
                     position="top-right"
