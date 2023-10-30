a3973d510908d5692bfe280c33e894f593f57f00
Riazul Islam <riazul5207@gmail.com>
2023-10-30 16:19:02 +0600

    navbar responsive completed💥

--- src/layouts/AppLayout.jsx
+++ src/layouts/AppLayout.jsx
@@ -11,7 +11,7 @@ const AppLayout = ({ children }) => {
         <>
             <Topbar/>
             <Header/>
-            {/* <Navbar/> */}
+            <Navbar/>
             {children}
             {/* <Footer/> */}
         </>
--- src/sections/Navbar.jsx
+++ src/sections/Navbar.jsx
@@ -16,27 +16,27 @@ const Navbar = () => {
     return (
         <div className="w-full">
             {/* nav links */}
-            <div className="flex items-center justify-between max-w-[1420px] mx-auto px-[0.4rem] py-[1.4rem]">
-                <div className="flex items-center justify-center">
-                    <NavLink to="/" className="navLink mr-[0.4rem] px-[0.6rem] py-[0.2rem] text-[#222] text-[1rem] font-[600] border-[1px] border-gray-600 rounded-md">Home</NavLink>
-                    <NavLink to="/groceries" className="navLink mr-[0.4rem] px-[0.6rem] py-[0.2rem] text-[#222] text-[1rem] font-[600] border-[1px] border-gray-600 rounded-md">Groceries</NavLink>
-                    {<NavLink to="/groceries/raw-products" className="navLink mr-[0.4rem] px-[0.6rem] py-[0.2rem] text-[#222] text-[1rem] font-[600] border-[1px] border-gray-600 rounded-md">Raw Products</NavLink>}
-                    {<NavLink to="/groceries/beverages" className="navLink mr-[0.4rem] px-[0.6rem] py-[0.2rem] text-[#222] text-[1rem] font-[600] border-[1px] border-gray-600 rounded-md">Beverages</NavLink>}
-                    {flag && <NavLink to="/groceries/raw-products/vegetables" className="navLink mr-[0.4rem] px-[0.6rem] py-[0.2rem] text-[#222] text-[1rem] font-[600] border-[1px] border-gray-600 rounded-md">Vegetables</NavLink>}
-                    {flag && <NavLink to="/groceries/raw-products/fruits" className="navLink mr-[0.4rem] px-[0.6rem] py-[0.2rem] text-[#222] text-[1rem] font-[600] border-[1px] border-gray-600 rounded-md">Fruits</NavLink>}
-                    {flag && <NavLink to="/groceries/raw-products/meat" className="navLink mr-[0.4rem] px-[0.6rem] py-[0.2rem] text-[#222] text-[1rem] font-[600] border-[1px] border-gray-600 rounded-md">Meat</NavLink>}
-                    {flag && <NavLink to="/groceries/raw-products/fish" className="navLink mr-[0.4rem] px-[0.6rem] py-[0.2rem] text-[#222] text-[1rem] font-[600] border-[1px] border-gray-600 rounded-md">Fish</NavLink>}
-                    {flag && <NavLink to="/groceries/raw-products/eggs" className="navLink mr-[0.4rem] px-[0.6rem] py-[0.2rem] text-[#222] text-[1rem] font-[600] border-[1px] border-gray-600 rounded-md">Eggs</NavLink>}
-                    {!flag && <NavLink to="/groceries/beverages/tea-coffe" className="navLink mr-[0.4rem] px-[0.6rem] py-[0.2rem] text-[#222] text-[1rem] font-[600] border-[1px] border-gray-600 rounded-md">Tea & Coffe</NavLink>}
-                    {flag && <NavLink to="/groceries/raw-products/spices" className="navLink mr-[0.4rem] px-[0.6rem] py-[0.2rem] text-[#222] text-[1rem] font-[600] border-[1px] border-gray-600 rounded-md">Spices</NavLink>}
-                    {!flag && <NavLink to="/groceries/beverages/dry-fruits" className="navLink mr-[0.4rem] px-[0.6rem] py-[0.2rem] text-[#222] text-[1rem] font-[600] border-[1px] border-gray-600 rounded-md">Dry Fruits</NavLink>}
-                    {!flag && <NavLink to="/groceries/beverages/jams" className="navLink mr-[0.4rem] px-[0.6rem] py-[0.2rem] text-[#222] text-[1rem] font-[600] border-[1px] border-gray-600 rounded-md">Jams & Jellies</NavLink>}
-                    {!flag && <NavLink to="/groceries/beverages/biscuits" className="navLink mr-[0.4rem] px-[0.6rem] py-[0.2rem] text-[#222] text-[1rem] font-[600] border-[1px] border-gray-600 rounded-md">Biscuits</NavLink>}
-                    {!flag && <NavLink to="/groceries/beverages/cakes" className="navLink mr-[0.4rem] px-[0.6rem] py-[0.2rem] text-[#222] text-[1rem] font-[600] border-[1px] border-gray-600 rounded-md">Cakes</NavLink>}
-                    {!flag && <NavLink to="/groceries/beverages/breads" className="navLink mr-[0.4rem] px-[0.6rem] py-[0.2rem] text-[#222] text-[1rem] font-[600] border-[1px] border-gray-600 rounded-md">Breads</NavLink>}
-                    {flag && <NavLink to="/groceries/raw-products/rice" className="navLink mr-[0.4rem] px-[0.6rem] py-[0.2rem] text-[#222] text-[1rem] font-[600] border-[1px] border-gray-600 rounded-md">Rice</NavLink>}
-                    {flag && <NavLink to="/groceries/raw-products/flour" className="navLink mr-[0.4rem] px-[0.6rem] py-[0.2rem] text-[#222] text-[1rem] font-[600] border-[1px] border-gray-600 rounded-md">Flour</NavLink>}
-                    {flag && <NavLink to="/groceries/raw-products/oil" className="navLink mr-[0.4rem] px-[0.6rem] py-[0.2rem] text-[#222] text-[1rem] font-[600] border-[1px] border-gray-600 rounded-md">Oil</NavLink>}
+            <div className=" mx-auto px-[0.4rem] py-[0.8rem] md:py-[1.4rem] flex items-center justify-between max-w-[1420px]">
+                <div className="flex flex-wrap items-center justify-center">
+                    <NavLink to="/" className="navLink mb-[0.4rem] mr-[0.4rem] px-[0.6rem] py-[0.2rem] text-[#222] text-[1rem] font-[600] border-[1px] border-gray-600 rounded-md">Home</NavLink>
+                    <NavLink to="/groceries" className="navLink mb-[0.4rem] mr-[0.4rem] px-[0.6rem] py-[0.2rem] text-[#222] text-[1rem] font-[600] border-[1px] border-gray-600 rounded-md">Groceries</NavLink>
+                    {<NavLink to="/groceries/raw-products" className="navLink mb-[0.4rem] mr-[0.4rem] px-[0.6rem] py-[0.2rem] text-[#222] text-[1rem] font-[600] border-[1px] border-gray-600 rounded-md">Raw Products</NavLink>}
+                    {<NavLink to="/groceries/beverages" className="navLink mb-[0.4rem] mr-[0.4rem] px-[0.6rem] py-[0.2rem] text-[#222] text-[1rem] font-[600] border-[1px] border-gray-600 rounded-md">Beverages</NavLink>}
+                    {flag && <NavLink to="/groceries/raw-products/vegetables" className="navLink mb-[0.4rem] mr-[0.4rem] px-[0.6rem] py-[0.2rem] text-[#222] text-[1rem] font-[600] border-[1px] border-gray-600 rounded-md">Vegetables</NavLink>}
+                    {flag && <NavLink to="/groceries/raw-products/fruits" className="navLink mb-[0.4rem] mr-[0.4rem] px-[0.6rem] py-[0.2rem] text-[#222] text-[1rem] font-[600] border-[1px] border-gray-600 rounded-md">Fruits</NavLink>}
+                    {flag && <NavLink to="/groceries/raw-products/meat" className="navLink mb-[0.4rem] mr-[0.4rem] px-[0.6rem] py-[0.2rem] text-[#222] text-[1rem] font-[600] border-[1px] border-gray-600 rounded-md">Meat</NavLink>}
+                    {flag && <NavLink to="/groceries/raw-products/fish" className="navLink mb-[0.4rem] mr-[0.4rem] px-[0.6rem] py-[0.2rem] text-[#222] text-[1rem] font-[600] border-[1px] border-gray-600 rounded-md">Fish</NavLink>}
+                    {flag && <NavLink to="/groceries/raw-products/eggs" className="navLink mb-[0.4rem] mr-[0.4rem] px-[0.6rem] py-[0.2rem] text-[#222] text-[1rem] font-[600] border-[1px] border-gray-600 rounded-md">Eggs</NavLink>}
+                    {!flag && <NavLink to="/groceries/beverages/tea-coffe" className="navLink mb-[0.4rem] mr-[0.4rem] px-[0.6rem] py-[0.2rem] text-[#222] text-[1rem] font-[600] border-[1px] border-gray-600 rounded-md">Tea & Coffe</NavLink>}
+                    {flag && <NavLink to="/groceries/raw-products/spices" className="navLink mb-[0.4rem] mr-[0.4rem] px-[0.6rem] py-[0.2rem] text-[#222] text-[1rem] font-[600] border-[1px] border-gray-600 rounded-md">Spices</NavLink>}
+                    {!flag && <NavLink to="/groceries/beverages/dry-fruits" className="navLink mb-[0.4rem] mr-[0.4rem] px-[0.6rem] py-[0.2rem] text-[#222] text-[1rem] font-[600] border-[1px] border-gray-600 rounded-md">Dry Fruits</NavLink>}
+                    {!flag && <NavLink to="/groceries/beverages/jams" className="navLink mb-[0.4rem] mr-[0.4rem] px-[0.6rem] py-[0.2rem] text-[#222] text-[1rem] font-[600] border-[1px] border-gray-600 rounded-md">Jams & Jellies</NavLink>}
+                    {!flag && <NavLink to="/groceries/beverages/biscuits" className="navLink mb-[0.4rem] mr-[0.4rem] px-[0.6rem] py-[0.2rem] text-[#222] text-[1rem] font-[600] border-[1px] border-gray-600 rounded-md">Biscuits</NavLink>}
+                    {!flag && <NavLink to="/groceries/beverages/cakes" className="navLink mb-[0.4rem] mr-[0.4rem] px-[0.6rem] py-[0.2rem] text-[#222] text-[1rem] font-[600] border-[1px] border-gray-600 rounded-md">Cakes</NavLink>}
+                    {!flag && <NavLink to="/groceries/beverages/breads" className="navLink mb-[0.4rem] mr-[0.4rem] px-[0.6rem] py-[0.2rem] text-[#222] text-[1rem] font-[600] border-[1px] border-gray-600 rounded-md">Breads</NavLink>}
+                    {flag && <NavLink to="/groceries/raw-products/rice" className="navLink mb-[0.4rem] mr-[0.4rem] px-[0.6rem] py-[0.2rem] text-[#222] text-[1rem] font-[600] border-[1px] border-gray-600 rounded-md">Rice</NavLink>}
+                    {flag && <NavLink to="/groceries/raw-products/flour" className="navLink mb-[0.4rem] mr-[0.4rem] px-[0.6rem] py-[0.2rem] text-[#222] text-[1rem] font-[600] border-[1px] border-gray-600 rounded-md">Flour</NavLink>}
+                    {flag && <NavLink to="/groceries/raw-products/oil" className="navLink mb-[0.4rem] mr-[0.4rem] px-[0.6rem] py-[0.2rem] text-[#222] text-[1rem] font-[600] border-[1px] border-gray-600 rounded-md">Oil</NavLink>}
                 </div>
             </div>
         </div>
