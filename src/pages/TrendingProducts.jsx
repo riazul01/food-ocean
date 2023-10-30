20c6246b15f660ab35fe8a97a86406a78297f2cb
Riazul Islam <riazul5207@gmail.com>
2023-10-30 13:32:30 +0600

    trending products page completed💥

--- src/pages/TrendingProducts.jsx
+++ src/pages/TrendingProducts.jsx
@@ -1,10 +1,17 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import AppLayout from '../layouts/AppLayout';
+import SectionProducts from '../sections/SectionProducts';
+import { ProductsContext } from '../context/ProductsContextProvider';
 
 const TrendingProducts = () => {
+    const { top, recent, popular } = useContext(ProductsContext);
+
     return (
-        <div>
-            <h1>Hello!</h1>
-        </div>
+        <AppLayout>
+            <SectionProducts title="top products" path="/groceries/top-products" products={top}/>
+            <SectionProducts title="recent products" path="/groceries/recent-products" products={recent}/>
+            <SectionProducts title="popular now" path="/groceries/popular-now" products={popular}/>
+        </AppLayout>
     );
 }
 
