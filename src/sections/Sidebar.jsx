a6b47f83d01f3df6b2b7678cc20aa88eea9c40e7
Riazul Islam <riazul5207@gmail.com>
2023-10-31 16:12:50 +0600

    sidebar section updated✈️

--- src/sections/Sidebar.jsx
+++ src/sections/Sidebar.jsx
@@ -1,9 +1,14 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link, NavLink, useLocation } from 'react-router-dom';
 
+// context
+import { CartContext } from '../context/CartContextProvider';
+
+// icons
 import { MdKeyboardArrowRight } from 'react-icons/md';
 
 const Sidebar = ({ toggle, setToggle }) => {
+    const { cartItems } = useContext(CartContext);
     const path = useLocation().pathname.split('/').pop();
     
     return (
@@ -35,9 +40,16 @@ const Sidebar = ({ toggle, setToggle }) => {
                         {path === "offers" && <MdKeyboardArrowRight className="text-[#fff] text-[1.3rem]"/>}
                         <span>New Offers</span>
                     </NavLink>
+                    <NavLink to="/cart" onClick={() => setToggle(false)} className={`${path === 'cart' ? 'bg-[#173334]' : null} px-[0.8rem] py-[0.4rem] text-[#fff] text-[1.1rem] flex items-center hover:bg-[#173334] rounded-lg`}>
+                        {path === "cart" && <MdKeyboardArrowRight className="text-[#fff] text-[1.3rem]"/>}
+                        <span>Cart Items</span>
+                        {cartItems.length > 0 && <div className="ms-[0.4rem] flex items-center justify-center h-[1.5rem] w-[1.5rem] rounded-[50%] bg-[orange]">
+                            <p className="text-[#fff] text-[0.8rem] font-bold">{cartItems.length}</p>
+                        </div>}
+                    </NavLink>
                     <NavLink to="/user/profile" onClick={() => setToggle(false)} className={`${(path === "login" || path === "register" || path === "profile" || path === "update-profile") ? 'bg-[#173334]' : null} px-[0.8rem] py-[0.4rem] text-[#fff] text-[1.1rem] flex items-center hover:bg-[#173334] rounded-lg`}>
                         {(path === "login" || path === "register" || path === "profile" || path === "update-profile") && <MdKeyboardArrowRight className="text-[#fff] text-[1.3rem]"/>}
-                        <span>Account</span>
+                        <span>My Account</span>
                     </NavLink>
                     <NavLink to="/user/orders" onClick={() => setToggle(false)} className={`${path === 'orders' ? 'bg-[#173334]' : null} px-[0.8rem] py-[0.4rem] text-[#fff] text-[1.1rem] flex items-center hover:bg-[#173334] rounded-lg`}>
                         {path === "orders" && <MdKeyboardArrowRight className="text-[#fff] text-[1.3rem]"/>}
