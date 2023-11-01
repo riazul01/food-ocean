5ebef16c2322522fa660d7a9f8dd4ad2dd722f19
Riazul Islam <riazul5207@gmail.com>
2023-11-01 14:12:50 +0600

    trending products page updated✈️

--- src/pages/TrendingProducts.jsx
+++ src/pages/TrendingProducts.jsx
@@ -14,9 +14,9 @@ const TrendingProducts = () => {
 
     return (
         <AppLayout>
-            <SectionProducts title="top products" path="/groceries/top-products" products={top}/>
-            <SectionProducts title="recent products" path="/groceries/recent-products" products={recent}/>
-            <SectionProducts title="popular now" path="/groceries/popular-now" products={popular}/>
+            <SectionProducts title="top products" path="/groceries/trending-now/top-products" products={top}/>
+            <SectionProducts title="recent products" path="/groceries/trending-now/recent-products" products={recent}/>
+            <SectionProducts title="popular now" path="/groceries/trending-now/popular-now" products={popular}/>
         </AppLayout>
     );
 }
