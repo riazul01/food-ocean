798452aa2059d4b5f22f603b51ab7a8b415910ea
Riazul Islam <riazul5207@gmail.com>
2023-11-08 20:30:38 +0600

    orders page responsive completed💥

--- src/pages/Orders.jsx
+++ src/pages/Orders.jsx
@@ -46,7 +46,7 @@ const Orders = () => {
                 <h1 className="pb-[0.2rem] text-[#173334] text-[1.6rem] font-[500] border-b-[1px] border-dashed border-[#ddd]">My Orders</h1>
 
                 {/* dashboard */}
-                <div className="mt-[1rem] w-full grid grid-cols-4 gap-[1.2rem]">
+                <div className="mt-[1rem] w-full grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-[1.2rem]">
                     <div className="w-full h-[120px] flex flex-col items-center justify-center bg-purple-500 rounded-lg">
                         <p className="text-[#fff] text-[1.8rem] font-[700] drop-shadow-lg">{totalOrders < 10 ? `0${totalOrders}` : totalOrders}</p>
                         <p className="text-[#fff] text-[1.2rem] font-[500] drop-shadow-lg">Total Orders</p>
@@ -69,10 +69,10 @@ const Orders = () => {
                 {ordersList && ordersList.length > 0 ? <table className="mt-[1.4rem] w-full rounded-lg overflow-hidden">
                     <thead className="bg-[#333]">
                         <tr className="">
-                            <th className="py-[0.4rem] text-[#fff] text-[1.1rem]">Order Id</th>
+                            <th className="hidden lg:table-cell py-[0.4rem] text-[#fff] text-[1.1rem]">Order Id</th>
                             <th className="py-[0.4rem] text-[#fff] text-[1.1rem]">Date</th>
-                            <th className="py-[0.4rem] text-[#fff] text-[1.1rem]">Total Cost</th>
-                            <th className="py-[0.4rem] text-[#fff] text-[1.1rem]">Status</th>
+                            <th className="hidden sm:table-cell py-[0.4rem] text-[#fff] text-[1.1rem]">Total Cost</th>
+                            <th className="hidden sm:table-cell py-[0.4rem] text-[#fff] text-[1.1rem]">Status</th>
                             <th className="py-[0.4rem] text-[#fff] text-[1.1rem]">Details</th>
                         </tr>
                     </thead>
@@ -80,10 +80,10 @@ const Orders = () => {
                         {ordersList.map((elem) => {
                             return (
                                 <tr className="text-center odd:bg-[#eeefef] even:bg-[#dddfef] border-b-[1px] border-b-[#ccc] last:border-b-0" key={elem.orderId}>
-                                    <td className="py-[0.4rem] text-[1.1rem]">{`#${elem.orderId.slice(0, 6)}`}</td>
+                                    <td className="hidden lg:table-cell py-[0.4rem] text-[1.1rem]">{`#${elem.orderId.slice(0, 6)}`}</td>
                                     <td className="py-[0.4rem] text-[1.1rem]">{elem.time.split(' ').slice(1, 4).join(' ')}</td>
-                                    <td className="py-[0.4rem] text-[1.1rem]">{elem.totalCost} Tk</td>
-                                    <td className="py-[0.4rem]"><span className={`${elem.status} px-[0.6rem] py-[0.1rem] text-[#fff] text-[1.1rem] rounded-md shadow-lg`}>{elem.status}</span></td>
+                                    <td className="hidden sm:table-cell py-[0.4rem] text-[1.1rem]">{elem.totalCost} Tk</td>
+                                    <td className="hidden sm:table-cell py-[0.4rem]"><span className={`${elem.status} px-[0.6rem] py-[0.1rem] text-[#fff] text-[1.1rem] rounded-md shadow-lg`}>{elem.status}</span></td>
                                     <td onClick={() => navigate(`/user/orders/${elem.orderId}`)} className="py-[0.4rem] text-[1.1rem] flex items-center justify-center cursor-pointer">
                                         <span className="text-green-900 text-[1.1rem] font-[600]">view details</span>
                                         <RxDoubleArrowRight className="ml-[0.2rem] text-green-900 text-[1.1rem] font-[600]" />
