53cf274cc58889ce5ddd0208c64aaa4858ca5ee3
Riazul Islam <riazul5207@gmail.com>
2023-11-08 21:17:04 +0600

    order details page responsive completed💥

--- src/pages/OrderDetails.jsx
+++ src/pages/OrderDetails.jsx
@@ -33,16 +33,16 @@ const OrderDetails = () => {
                         </div>
 
                         <div className="px-[0.4rem] py-[0.2rem] border-[1px] border-[#ccc] border-t-0 rounded-b-lg">
-                            <p className="py-[0.1rem] flex items-center">
-                                <strong className="text-[1.1rem] font-[600] text-green-900 w-[150px]">Order Id</strong>
+                            <p className="py-[0.1rem] flex flex-col sm:flex-row sm:items-center border-b-[1px] border-b-[#ccc] border-dashed sm:border-b-0 last:border-b-0">
+                                <strong className="text-[1.1rem] font-[600] text-green-900 w-full sm:w-[150px]">Order Id</strong>
                                 <span className="text-[1.1rem]">{orderDetails.orderId}</span>
                             </p>
-                            <p className="py-[0.1rem] flex items-center">
-                                <strong className="text-[1.1rem] font-[600] text-green-900 w-[150px]">Order Date</strong>
+                            <p className="py-[0.1rem] flex flex-col sm:flex-row sm:items-center border-b-[1px] border-b-[#ccc] border-dashed sm:border-b-0 last:border-b-0">
+                                <strong className="text-[1.1rem] font-[600] text-green-900 w-full sm:w-[150px]">Order Date</strong>
                                 <span className="text-[1.1rem]">{orderDetails.time}</span>
                             </p>
-                            <p className="py-[0.1rem] flex items-center">
-                                <strong className="text-[1.1rem] font-[600] text-green-900 w-[150px]">Order Status</strong>
+                            <p className="py-[0.1rem] flex flex-col sm:flex-row sm:items-center border-b-[1px] border-b-[#ccc] border-dashed sm:border-b-0 last:border-b-0">
+                                <strong className="text-[1.1rem] font-[600] text-green-900 w-full sm:w-[150px]">Order Status</strong>
                                 <span className="text-[1.1rem]">{orderDetails.status}</span>
                             </p>
                         </div>
@@ -54,16 +54,16 @@ const OrderDetails = () => {
                         </div>
 
                         <div className="px-[0.4rem] py-[0.2rem] border-[1px] border-[#ccc] border-t-0 rounded-b-lg">
-                            <p className="py-[0.1rem] flex items-center">
-                                <strong className="text-[1.1rem] font-[600] text-green-900 w-[150px]">Email</strong>
+                            <p className="py-[0.1rem] flex flex-col sm:flex-row sm:items-center border-b-[1px] border-b-[#ccc] border-dashed sm:border-b-0 last:border-b-0">
+                                <strong className="text-[1.1rem] font-[600] text-green-900 w-full sm:w-[150px]">Email</strong>
                                 <span className="text-[1.1rem]">{orderDetails.shippingInfo.email}</span>
                             </p>
-                            <p className="py-[0.1rem] flex items-center">
-                                <strong className="text-[1.1rem] font-[600] text-green-900 w-[150px]">Phone</strong>
+                            <p className="py-[0.1rem] flex flex-col sm:flex-row sm:items-center border-b-[1px] border-b-[#ccc] border-dashed sm:border-b-0 last:border-b-0">
+                                <strong className="text-[1.1rem] font-[600] text-green-900 w-full sm:w-[150px]">Phone</strong>
                                 <span className="text-[1.1rem]">{orderDetails.shippingInfo.phone}</span>
                             </p>
-                            <p className="py-[0.1rem] flex items-center">
-                                <strong className="text-[1.1rem] font-[600] text-green-900 w-[150px]">Address</strong>
+                            <p className="py-[0.1rem] flex flex-col sm:flex-row sm:items-center border-b-[1px] border-b-[#ccc] border-dashed sm:border-b-0 last:border-b-0">
+                                <strong className="text-[1.1rem] font-[600] text-green-900 w-full sm:w-[150px]">Address</strong>
                                 <span className="text-[1.1rem] capitalize">
                                     {orderDetails.shippingInfo.address.street},&nbsp;
                                     {orderDetails.shippingInfo.address.city}-{orderDetails.shippingInfo.address.postcode},&nbsp; 
@@ -83,29 +83,29 @@ const OrderDetails = () => {
                             {orderDetails.orderItems.map((elem, index) => {
                                 return (
                                     <div  key={index} className="px-[0.4rem] py-[0.2rem] flex items-start justify-between border-b-[1px] border-b-[#ccc] last:border-b-0">
-                                        <div className="">
-                                            <p className="py-[0.1rem] flex items-center">
-                                                <strong className="text-[1.1rem] font-[600] text-green-900 w-[150px]">Name</strong>
+                                        <div className="w-[calc(100%-130px)]">
+                                            <p className="py-[0.1rem] flex flex-col sm:flex-row sm:items-center border-b-[1px] border-b-[#ccc] border-dashed sm:border-b-0 last:border-b-0">
+                                                <strong className="text-[1.1rem] font-[600] text-green-900 w-full sm:w-[150px]">Name</strong>
                                                 <span className="text-[1.1rem]">{elem.name}</span>
                                             </p>
-                                            <p className="py-[0.1rem] flex items-center">
-                                                <strong className="text-[1.1rem] font-[600] text-green-900 w-[150px]">Category</strong>
+                                            <p className="py-[0.1rem] flex flex-col sm:flex-row sm:items-center border-b-[1px] border-b-[#ccc] border-dashed sm:border-b-0 last:border-b-0">
+                                                <strong className="text-[1.1rem] font-[600] text-green-900 w-full sm:w-[150px]">Category</strong>
                                                 <span className="text-[1.1rem]">{elem.category}</span>
                                             </p>
-                                            <p className="py-[0.1rem] flex items-center">
-                                                <strong className="text-[1.1rem] font-[600] text-green-900 w-[150px]">Price</strong>
+                                            <p className="py-[0.1rem] flex flex-col sm:flex-row sm:items-center border-b-[1px] border-b-[#ccc] border-dashed sm:border-b-0 last:border-b-0">
+                                                <strong className="text-[1.1rem] font-[600] text-green-900 w-full sm:w-[150px]">Price</strong>
                                                 <span className="text-[1.1rem]">{parseInt(elem.price - ((elem.price * elem.discount) / 100))} Tk</span>
                                             </p>
-                                            <p className="py-[0.1rem] flex items-center">
-                                                <strong className="text-[1.1rem] font-[600] text-green-900 w-[150px]">Discount</strong>
+                                            <p className="py-[0.1rem] flex flex-col sm:flex-row sm:items-center border-b-[1px] border-b-[#ccc] border-dashed sm:border-b-0 last:border-b-0">
+                                                <strong className="text-[1.1rem] font-[600] text-green-900 w-full sm:w-[150px]">Discount</strong>
                                                 <span className="text-[1.1rem]">{elem.discount}%</span>
                                             </p>
-                                            <p className="py-[0.1rem] flex items-center">
-                                                <strong className="text-[1.1rem] font-[600] text-green-900 w-[150px]">Weight</strong>
+                                            <p className="py-[0.1rem] flex flex-col sm:flex-row sm:items-center border-b-[1px] border-b-[#ccc] border-dashed sm:border-b-0 last:border-b-0">
+                                                <strong className="text-[1.1rem] font-[600] text-green-900 w-full sm:w-[150px]">Weight</strong>
                                                 <span className="text-[1.1rem]">{`${elem.weight}${elem.unit} * ${elem.cartQuantity}`}</span>
                                             </p>
-                                            <p className="py-[0.1rem] flex items-center">
-                                                <strong className="text-[1.1rem] font-[600] text-green-900 w-[150px]">Total Price</strong>
+                                            <p className="py-[0.1rem] flex flex-col sm:flex-row sm:items-center border-b-[1px] border-b-[#ccc] border-dashed sm:border-b-0 last:border-b-0">
+                                                <strong className="text-[1.1rem] font-[600] text-green-900 w-full sm:w-[150px]">Total Price</strong>
                                                 <span className="text-[1.1rem]">{parseInt(elem.price - ((elem.price * elem.discount) / 100)) * elem.cartQuantity} Tk</span>
                                             </p>
                                         </div>
@@ -124,20 +124,20 @@ const OrderDetails = () => {
                         </div>
 
                         <div className="px-[0.4rem] py-[0.2rem] border-[1px] border-[#ccc] border-t-0 rounded-b-lg">
-                            <p className="py-[0.1rem] flex items-center">
-                                <strong className="text-[1.1rem] font-[600] text-green-900 w-[150px]">Sub Total</strong>
+                            <p className="py-[0.1rem] flex flex-col sm:flex-row sm:items-center border-b-[1px] border-b-[#ccc] border-dashed sm:border-b-0 last:border-b-0">
+                                <strong className="text-[1.1rem] font-[600] text-green-900 w-full sm:w-[150px]">Sub Total</strong>
                                 <span className="text-[1.1rem]">{orderDetails.subTotal} Tk</span>
                             </p>
-                            <p className="py-[0.1rem] flex items-center">
-                                <strong className="text-[1.1rem] font-[600] text-green-900 w-[150px]">Discount</strong>
+                            <p className="py-[0.1rem] flex flex-col sm:flex-row sm:items-center border-b-[1px] border-b-[#ccc] border-dashed sm:border-b-0 last:border-b-0">
+                                <strong className="text-[1.1rem] font-[600] text-green-900 w-full sm:w-[150px]">Discount</strong>
                                 <span className="text-[1.1rem]">{orderDetails.discount} Tk</span>
                             </p>
-                            <p className="py-[0.1rem] flex items-center">
-                                <strong className="text-[1.1rem] font-[600] text-green-900 w-[150px]">Shipping Cost</strong>
+                            <p className="py-[0.1rem] flex flex-col sm:flex-row sm:items-center border-b-[1px] border-b-[#ccc] border-dashed sm:border-b-0 last:border-b-0">
+                                <strong className="text-[1.1rem] font-[600] text-green-900 w-full sm:w-[150px]">Shipping Cost</strong>
                                 <span className="text-[1.1rem]">{orderDetails.shippingCost} Tk</span>
                             </p>
-                            <p className="py-[0.1rem] flex items-center">
-                                <strong className="text-[1.1rem] font-[600] text-green-900 w-[150px]">Total Amount</strong>
+                            <p className="py-[0.1rem] flex flex-col sm:flex-row sm:items-center border-b-[1px] border-b-[#ccc] border-dashed sm:border-b-0 last:border-b-0">
+                                <strong className="text-[1.1rem] font-[600] text-green-900 w-full sm:w-[150px]">Total Amount</strong>
                                 <span className="text-[1.1rem]">{orderDetails.totalCost} Tk</span>
                             </p>
                         </div>
@@ -149,8 +149,8 @@ const OrderDetails = () => {
                         </div>
 
                         <div className="px-[0.4rem] py-[0.2rem] border-[1px] border-[#ccc] border-t-0 rounded-b-lg">
-                            <p className="py-[0.1rem] flex items-center">
-                                <strong className="text-[1.1rem] font-[600] text-green-900 w-[150px]">Method</strong>
+                            <p className="py-[0.1rem] flex flex-col sm:flex-row sm:items-center border-b-[1px] border-b-[#ccc] border-dashed sm:border-b-0 last:border-b-0">
+                                <strong className="text-[1.1rem] font-[600] text-green-900 w-full sm:w-[150px]">Method</strong>
                                 <span className="text-[1.1rem]">COD</span>
                             </p>
                         </div>
