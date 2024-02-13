beba3a9cdb48cf7ec98316e63fcf4c0d71164f20
Riazul Islam <riazul5207@gmail.com>
2024-02-13 11:41:09 +0600

    product card ui updated✈️

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -48,7 +48,7 @@ const ProductCard = ({ data }) => {
                     {data.discount !== '0' && <del className="mr-[0.4rem] text-[#888] text-[1.1rem] font-bold">{data.price} Tk</del>}
                     <ins className="text-[#0c2a2b] text-[1.1rem] font-bold no-underline">{parseInt(data.price - (data.price * (data.discount / 100)))} Tk</ins>
                 </p>
-                <button onClick={handleAddToCart} className={`absolute left-0 bottom-0 py-[0.4rem] ${addedToCart ? 'w-[45%]': 'w-full'} bg-[#0c2a2b] text-[1.1rem] text-[orange] font-[500] outline-none rounded-sm z-[99]`}>{addedToCart ? 'Added👍' : 'Add to Cart 🛒'}</button>
+                <button onClick={handleAddToCart} className={`absolute left-0 bottom-0 py-[0.4rem] ${addedToCart ? 'w-[45%]': 'w-full'} bg-[#0c2a2b] text-[1.1rem] text-[orange] font-[500] outline-none rounded-sm z-[99]`}>{addedToCart ? 'Added' : 'Add to Cart'}</button>
                 <div className={`absolute right-0 bottom-0 flex items-center`}>
                     <button onClick={handleDecrement} className="px-[0.7rem] py-[0.62rem] border-none outline-none bg-[#222] rounded-sm"><HiMinus className="text-white text-[1.2rem]"/></button>
                     <span className="text-[1.2rem] text-center font-bold w-[2.5rem]">{`${(cartItem !== undefined) ? cartItem.quantity : 0}`}</span>
